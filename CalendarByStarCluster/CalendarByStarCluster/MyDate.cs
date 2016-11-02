c12e1d72282c5fb2b3a58def68e2d9330b3919d1
kuranadomoe <Wearestardust404@outlook.com>
2016-11-02 22:51:32 +0800

    comment

--- CalendarByStarCluster/CalendarByStarCluster/MyDate.cs
+++ CalendarByStarCluster/CalendarByStarCluster/MyDate.cs
@@ -27,6 +27,13 @@ namespace CalendarByStarCluster
         }
 
 
+        /// <summary>
+        /// 判断是不是格里历(1582年10月15日以后)
+        /// </summary>
+        /// <param name="year">年</param>
+        /// <param name="month">月</param>
+        /// <param name="day">日</param>
+        /// <returns>是则返回真,否则返回假</returns>
         public static bool IsGregorianCalendar(int year,int month,int day)
         {
             if (year > 1582 || (year == 1582 && month >= 10 && day >= 15))
@@ -36,25 +43,42 @@ namespace CalendarByStarCluster
         }
 
 
+        /// <summary>
+        /// 判断是不是闰年
+        /// </summary>
+        /// <param name="year">要判断的年份</param>
+        /// <returns>是则返回真,否则返回假</returns>
         public static bool IsLeapYear(int year)
         {
             return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
         }
 
 
-        private int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        /// <summary>
+        /// 平年每月的天数
+        /// </summary>
+        private static int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        /// <summary>
+        /// 返回当前实例当月最大天数
+        /// </summary>
         public int MonthDays
         {
             get { return (IsLeapYear(year) ? 1 : 0) + days[month - 1]; }
         }
 
 
+        /// <summary>
+        /// 儒略日
+        /// </summary>
         public int JulianDay
         {
             get { return (153 * month + 2) / 5 + 365 * year + day + 32045 + year / 4 - year / 100 + year / 400; }
         }
 
 
+        /// <summary>
+        /// 星期几
+        /// </summary>
         public byte Week
         {
             get { return (byte)(JulianDay % 7); }
