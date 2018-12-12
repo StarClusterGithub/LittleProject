534710c0b5e74c787e7c703b1d881a9e919e81e6
kuranadomoe <Wearestardust404@outlook.com>
2018-12-12 23:49:41 +0800

    maxthon bookmark covert to chrome bookmark

---
+++ SmallTools/MaxthonChromeBookmarkConverter/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace MaxthonChromeBookmarkConverter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbImport = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.rtbShowInfo = new System.Windows.Forms.RichTextBox();
+            this.tbExport = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbImport
+            // 
+            this.tbImport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbImport.Location = new System.Drawing.Point(12, 12);
+            this.tbImport.Name = "tbImport";
+            this.tbImport.Size = new System.Drawing.Size(222, 21);
+            this.tbImport.TabIndex = 1;
+            this.tbImport.Text = "Please input Mathon bookmark directory...";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImport.Location = new System.Drawing.Point(241, 13);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(91, 23);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            // 
+            // rtbShowInfo
+            // 
+            this.rtbShowInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbShowInfo.Location = new System.Drawing.Point(12, 56);
+            this.rtbShowInfo.Name = "rtbShowInfo";
+            this.rtbShowInfo.ReadOnly = true;
+            this.rtbShowInfo.Size = new System.Drawing.Size(320, 179);
+            this.rtbShowInfo.TabIndex = 2;
+            this.rtbShowInfo.Text = "";
+            // 
+            // tbExport
+            // 
+            this.tbExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbExport.Location = new System.Drawing.Point(12, 262);
+            this.tbExport.Name = "tbExport";
+            this.tbExport.Size = new System.Drawing.Size(222, 21);
+            this.tbExport.TabIndex = 3;
+            this.tbExport.Text = "Plaese input export directory...";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(241, 262);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(91, 23);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 321);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.tbExport);
+            this.Controls.Add(this.rtbShowInfo);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.tbImport);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Maxthon/Chrome Bookmark Coverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbImport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.RichTextBox rtbShowInfo;
+        private System.Windows.Forms.TextBox tbExport;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ SmallTools/MaxthonChromeBookmarkConverter/MainForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MaxthonChromeBookmarkConverter
+{
+    public partial class MainForm : Form
+    {
+        private static string flag = "##IMPORT##\n";
+
+        /// <summary>
+        /// chrome书签格式
+        /// </summary>
+        private string Bookmark { get; set; } =
+            "<!DOCTYPE NETSCAPE-Bookmark-file-1>\n" +
+            "<!-- This is an automatically generated file.\n" +
+            "     It will be read and overwritten.\n" +
+            "     DO NOT EDIT! -->\n" +
+            "<META HTTP-EQUIV=\"Content - Type\" CONTENT=\"text / html; charset = UTF - 8\">\n" +
+             "<TITLE>Bookmarks</TITLE>\n" +
+            "<H1>Bookmarks</H1>\n" +
+            "<DL><p>\n" +
+            "    <DT><H3 ADD_DATE=\"1519379300\" LAST_MODIFIED=\"1544504910\" PERSONAL_TOOLBAR_FOLDER=\"true\">书签栏</H3>\n" +
+            "    <DL><p>\n" +
+             flag +
+            "    </DL><p>\n" +
+            "</DL><p>\n";
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.btnImport.Click += BtnImport_Click;
+            this.btnExport.Click += BtnExport_Click;
+            this.tbImport.KeyDown += TbImport_KeyDown;
+            this.tbExport.KeyDown += TbExport_KeyDown;
+
+            this.tbImport.Enter += (x, y) => (x as TextBox).Text = "";
+            this.tbExport.Enter += (x, y) => (x as TextBox).Text = "";
+
+            this.rtbShowInfo.AppendText(Bookmark);
+        }
+
+        /// <summary>
+        /// 弹出选择目录对话框,并返回选择的目录路径,若未选择则返回null
+        /// </summary>
+        /// <returns>directory path or null</returns>
+        private string SelectDir()
+        {
+            string dir = null;
+
+            FolderBrowserDialog selDir = new FolderBrowserDialog();
+            selDir.Description = "Please select directory";
+
+            if (selDir.ShowDialog() == DialogResult.OK)
+                dir = selDir.SelectedPath;
+
+            return dir;
+        }
+
+        /// <summary>
+        /// 从指定目录导入Maxthon书签,并将其转为html格式的chrome书签
+        /// </summary>
+        /// <param name="dir">maxthon书签目录</param>
+        /// <param name="indentation">缩进字符串</param>
+        /// <returns>chrome书签的html描述格式(不包含头和尾)</returns>
+        private string Import(string dir, string indentation = "    ")
+        {
+            var bookmark = "";
+
+            DirectoryInfo bookmarkInfo = new DirectoryInfo(dir);
+            foreach (var itm in bookmarkInfo.GetFileSystemInfos())
+            {
+                long timesetamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                switch (itm.Attributes)
+                {
+                    case FileAttributes.Archive:
+                        var url = File.ReadAllText(itm.FullName).Trim('\r', '\n');
+                        url = url.Substring(url.IndexOf("URL=") + 4);
+                        bookmark += $"<DT><A HREF=\"{url}\" ADD_DATE=\"{timesetamp}\">{Path.GetFileNameWithoutExtension(itm.Name)}</A>\n";
+                        break;
+                    case FileAttributes.Directory:
+                        bookmark += $"<DT><H3 ADD_DATE=\"{timesetamp} \" LAST_MODIFIED=\"{timesetamp} \">{itm.Name}</H3>\n";
+                        var tmp = Import(itm.FullName);
+                        bookmark += $"<DL><p>\n{tmp}</DL><p>\n";
+                        break;
+                    default:
+                        throw new FileLoadException("Unexpected file type!");
+                }
+            }
+
+            //  Indentation
+            bookmark = string.Join("\n" + indentation, bookmark.Split('\n'));
+            bookmark = indentation + bookmark.TrimEnd(' ');
+
+            return bookmark;
+        }
+
+        /// <summary>
+        /// 将结果保存至指定目录
+        /// </summary>
+        /// <param name="dir">目录路径</param>
+        private void Export(string dir)
+        {
+            string fileName = "bookmarks_" + DateTime.Now.ToString("yyyy_MM_dd") + ".html";
+            File.WriteAllText(Path.Combine(dir,fileName), Bookmark);
+        }
+
+        private void TbImport_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var dir = (sender as TextBox).Text;
+                if (Directory.Exists(dir))
+                {
+                    Bookmark = Bookmark.Replace(flag, Import(dir, "        "));
+                    rtbShowInfo.Text = Bookmark;
+                }
+            }
+        }
+
+        private void BtnImport_Click(object sender, EventArgs e)
+        {
+            var dir = SelectDir();
+            if (dir != null)
+            {
+                tbImport.Text = dir;
+                Bookmark = Bookmark.Replace(flag, Import(dir, "        "));
+                rtbShowInfo.Text = Bookmark;
+            }
+        }
+
+        private void TbExport_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                var dir = (sender as TextBox).Text;
+                if (Directory.Exists(dir))
+                {
+                    Export(dir);
+                }
+                else
+                {
+                    MessageBox.Show("Not a valid directory!");
+                }
+            }
+        }
+
+        private void BtnExport_Click(object sender, EventArgs e)
+        {
+            var dir = SelectDir();
+            if (dir != null)
+            {
+                tbExport.Text = dir;
+                Export(dir);
+            }
+        }
+    }
+}
